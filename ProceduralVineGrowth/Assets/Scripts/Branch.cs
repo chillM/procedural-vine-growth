e1ee4d46b9eeb3af9c0b6cd76773873bc48d7c05
Colin Hill <cxmhill@gmail.com>
2019-11-29 16:19:39 -0600

    Moved Unity Project into Repository
    Initialize the repository with the project as it currently stands

---
+++ ProceduralVineGrowth/Assets/Scripts/Branch.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossSection
+{
+    public Vector3 position;
+    public Vector3 normal;
+    public float radius;
+    public Vector3[] vertices { get; private set; }
+    public bool changeOccurs;
+
+    /// <summary>
+    /// Uses the position, plane normal, and radius to calculate the vertices for this cross section
+    /// </summary>
+    /// <param name="numSides">Decides how many points should be created</param>
+    public void calculateVertices(int numSides) {
+        // TODO implement vertex calculation
+    }
+}
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class Branch : MonoBehaviour
+{
+    public Vine vine; // a reference to the parent vine that defines specific branch parameters
+
+    private MeshFilter filter;
+    private List<CrossSection> crossSections;
+
+    void Start()
+    {
+        filter = gameObject.GetComponent<MeshFilter>();
+
+        //TODO initialize the mesh with one cross section (or two)
+    }
+
+    /// <summary>
+    /// Update the branch mesh
+    /// </summary>
+    /// <param name="deltaTime">Controls how much change occurs</param>
+    void updateBranch(float deltaTime) {
+        for (int i = 1; i < crossSections.Count; i++)
+        {
+            crossSections[i].changeOccurs = false;
+            //check distance
+            if(Vector3.Distance(crossSections[i].position, crossSections[i-1].position) < vine.sectionLength) {
+                // TODO increase the distance of the cross section
+                crossSections[i].changeOccurs = true;
+            }
+            //check radius
+            if(crossSections[i].radius < vine.maxRadius) {
+                // TODO increase the radius of the cross section
+                crossSections[i].changeOccurs = true;
+            }
+
+            if(crossSections[i].changeOccurs) {
+                crossSections[i].calculateVertices(vine.sides);
+            }
+
+            //the triangles array should only be modified when new points are added, and should only be appended to
+
+            // TODO add the cross section vertices to the vertex array
+        }
+    }
+}
---
+++ ProceduralVineGrowth/Assets/Scripts/Vine.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vine : MonoBehaviour
+{
+    public float sectionLength { get; private set; }
+    public float maxRadius { get; private set; }
+    public int sides { get; private set; } //controls how many sides the mesh cylinder has
+
+}
