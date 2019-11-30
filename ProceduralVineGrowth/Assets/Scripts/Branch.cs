c380639ff9fc7e1f06482b9dde531b5ee0978826
Colin Hill <cxmhill@gmail.com>
2019-11-30 01:31:07 -0600

    Added a Test for Mesh Calculation
    Added a test scene that tests the cross section connection functionality to check whether meshes are being properly generated.

--- ProceduralVineGrowth/Assets/Scripts/Branch.cs
+++ ProceduralVineGrowth/Assets/Scripts/Branch.cs
@@ -8,14 +8,54 @@ public class CrossSection
     public Vector3 normal;
     public float radius;
     public Vector3[] vertices { get; private set; }
+    public Vector3[] normals { get; private set; }
     public bool changeOccurs;
+
+    CrossSection(Vector3 position, Vector3 normal, float radius) {
+        this.position = position;
+        this.normal = normal;
+        this.radius = radius;
+    }
 
     /// <summary>
     /// Uses the position, plane normal, and radius to calculate the vertices for this cross section
     /// </summary>
     /// <param name="numSides">Decides how many points should be created</param>
-    public void calculateVertices(int numSides) {
-        // TODO implement vertex calculation
+    public void CalculateVertices(int numSides) {
+        Vector3 temp = Vector3.Angle(normal, Vector3.right) > 0.1f ? Vector3.Cross(normal, Vector3.right) : Vector3.Cross(normal, Vector3.forward);
+        temp.Normalize();
+        temp *= radius;
+        float _2pi = Mathf.PI * 2f;
+
+        vertices = new Vector3[numSides*2];
+        int vert = 0;
+
+        //first point
+        vertices[0] = position + temp;
+        //all other points (entered twice)
+        for(int v = 1; v < numSides; v++) {
+            //rotate the temp vector and get the point there
+            temp = Quaternion.AngleAxis(1/numSides * _2pi, normal) * temp; //rotate temp 1/numSides around the normal vector
+            vertices[vert++] = position + temp;
+
+        }
+        //last point
+        vertices[vertices.Length-1] = vertices[0];
+
+
+        // TODO also include normal calculation
+    }
+
+    /// <summary>
+    /// Connects two cross sections together assuming they are adjacent in a vertex array
+    /// </summary>
+    /// <param name="prev"></param>
+    /// <param name="current"></param>
+    /// <returns>The triangle list that would result assuming prev was index 0 of the vertex array</returns>
+    public static List<int> ConnectSections(CrossSection prev, CrossSection current) {
+        List<int> triangles = new List<int>();
+
+        return triangles;
     }
 }
 
@@ -39,7 +79,7 @@ public class Branch : MonoBehaviour
     /// Update the branch mesh
     /// </summary>
     /// <param name="deltaTime">Controls how much change occurs</param>
-    void updateBranch(float deltaTime) {
+    void UpdateBranch(float deltaTime) {
         for (int i = 1; i < crossSections.Count; i++)
         {
             crossSections[i].changeOccurs = false;
@@ -55,12 +95,19 @@ public class Branch : MonoBehaviour
             }
 
             if(crossSections[i].changeOccurs) {
-                crossSections[i].calculateVertices(vine.sides);
+                crossSections[i].CalculateVertices(vine.sides);
             }
 
             //the triangles array should only be modified when new points are added, and should only be appended to
 
             // TODO add the cross section vertices to the vertex array
+            
+            if(i == crossSections.Count - 1) {
+                // TODO look at the last cross section; if it has reached a full section length and there is space, then create a new cross section
+
+                //add new faces to the triangle array
+            }
         }
+
     }
 }
---
+++ ProceduralVineGrowth/Assets/Scripts/TestMesh.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMesh : MonoBehaviour
+{
+    public int numSides = 4;
+
+    void Start()
+    {
+        // test the cross section connection using a cube shape
+        List<CrossSection> sections = new List<CrossSection>();
+
+        //insert elements
+        InsertElements(sections);
+
+        //build a mesh
+        BuildMesh(sections);
+    }
+
+    private void BuildMesh(List<CrossSection> sections) {
+        Vector3[] vertices = new Vector3[sections.Count * numSides * 2];
+        Vector3[] normals = new Vector3[vertices.Length];
+        int vert = 0;
+        List<int> triangles = new List<int>();
+
+        sections[0].CalculateVertices(numSides);
+        foreach (Vector3 point in sections[0].vertices)
+        {
+            vertices[vert++] = point;
+        }
+        // TODO add the cap on the end
+        for(int i = 1; i < sections.Count; i++) {
+            sections[i].CalculateVertices(numSides);
+            // add the vertices to the vertex array
+            foreach (Vector3 point in sections[i].vertices)
+            {
+                vertices[vert++] = point;
+            }
+
+            // calculate the faces
+            List<int> tempList = CrossSection.ConnectSections(sections[i-1], sections[i]);
+            // adjust the triangle values returned
+            for(int j = 0; j < tempList.Count; j++) {
+                tempList[j] += vert - 2*numSides; // adding where the previous cross section starts in the vertex array
+            }
+            triangles.AddRange(tempList); // add the new triangles to the list
+
+            // add the normals
+
+        }
+        // TODO add the cap on the end
+    }
+
+    private void InsertElements(List<CrossSection> list) {
+
+    }
+}
